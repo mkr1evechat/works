aaa105f482a62bbf65e3c35301bcdd40ad537b8f
매띵 mkr1 <mkr1.evechat@gmail.com>
2026-01-21 22:59:50 -0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -10,12 +10,6 @@
      // 👇 불필요한 배경색 클래스 제거
      <div>
        <div className="container mx-auto px-4 sm:px-6 lg:px-8 py-8 lg:py-12">
-         <header className="flex justify-between items-center mb-8 lg:mb-12">
-           <div>
-             {/* 👇 텍스트에 dark: 접두사 적용 */}
-           </div>
-         </header>
-
          <WorkGallery allWorks={allWorks} />
        </div>
      </div>
